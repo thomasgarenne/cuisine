0e7a0b2b3806791bac88224994537b1094446268
thomas <thomas.garenne@outlook.com>
2023-11-28 15:45:22 +0100

    vendor/autoload

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../.env';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
